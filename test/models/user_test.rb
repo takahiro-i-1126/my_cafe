ee5c016557e43b38887dc1eada56b155eca0e3c2
takahiro_i <takahiro.git@gmail.com>
2020-11-19 07:18:57 +0900

    creare user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+def setup
+  @user = User.new(name: "test user", email: "user@example.com",
+                   password: "foobar", password_confirmation: "foobar")
+end
+
+test "should user valid" do
+  assert @user.valid?
+end
+
+test "name should be presence" do
+  @user.name = " "
+  assert_not @user.valid?
+end
+
+test "emali should be presence" do
+  @user.email = " "
+  assert_not @user.valid?
+end
+
+test "name should not be too long" do
+   @user.name = "a" * 51
+   assert_not @user.valid?
+ end
+
+ test "email should not be too long" do
+   @user.email = "a" * 244 + "@example.com"
+   assert_not @user.valid?
+ end
+
+ test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+end
