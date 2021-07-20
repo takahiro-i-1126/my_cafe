fc36fadf4605e4b0184b5282c13c3a5e7df61bc2
takahiro_i <takahiro.git@gmail.com>
2021-07-21 06:27:16 +0900

    outline store registion

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,6 @@
 class StoresController < ApplicationController
   def new
+    @store = Store.new
   end
 
   def show
@@ -7,4 +8,19 @@ class StoresController < ApplicationController
     @review = current_user.reviews.build if logged_in?
     @reviews = @store.reviews.paginate(page: params[:page])
   end
+
+def create
+    @store = Store.new(store_params)    # 実装は終わっていないことに注意!
+    if @store.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def store_params
+      params.require(:store).permit(:name, :title, :introduction)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,21 @@ User.create!(id: 1,
              password:              "foobar",
              password_confirmation: "foobar")
 
+User.create!(id: 2,
+             name:  "Test User",
+             email: "test@gmail.com",
+             password:              "061126",
+             password_confirmation: "061126")
+
 Store.create!(id: 1,
               name: "starbucks",
               title: "welcome",
               introduction: "Coffee shop")
+
+Store.create!(id: 2,
+              name: "momocafe",
+              title: "マイホームの雰囲気がある喫茶店です",
+              introduction: "パンケーキがおすすめです")
 
 @user = User.find(1)
 @store = Store.find(1)
